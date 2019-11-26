44d862bdd094e53e80da748a463873d00bc91d32
EdMSL <eduard2011rus@gmail.com>
2019-11-26 14:57:47 +0700

    Updates

--- config/webpack/plugins/mini-css-extract-plugin.js
+++
@@ -1,8 +0,0 @@
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-
-module.exports = function() {
-  return new MiniCssExtractPlugin({
-    filename: 'css/styles.css',
-    chunkFilename: 'css/[id].css',
-  });
-}
--- src/index.js
+++
@@ -1,2 +0,0 @@
-require('./js/main');
-require('$styles/styles.scss');
